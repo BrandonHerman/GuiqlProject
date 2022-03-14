548b054bb9b9b2f856de4865a7b1914c9cefd4a5
BrandonHerman <brandonherman0202@gmail.com>
2022-03-14 17:55:40 -0500

    Front-end sign in, student+instructor/signup. Forgot to branch.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import StudentSignIn from './pages/signinviews/studentsignin';
+import { Link } from "react-router-dom";
 
 // React functional component
 function App () {
@@ -68,22 +70,10 @@ function App () {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  return (
+  return ( //launch app at student sign in 
     <div className="App">
-      <header className="App-header">
-        <h2>Working Name? I'm thinking Guiql</h2>
-        <button onClick={fetchBase} style={{marginBottom: '1rem'}}> {`GET: http://${url}:8000/`} </button>
-        <button onClick={reset}> Reset DB </button>
-        <form onSubmit={handleSubmit}>
-          <input type="text" value={number} onChange={handleChange}/>
-          <br/>
-          <input type="submit" value="Submit" />
-        </form>
-        <ul>
-          { values.map((value, i) => <li key={i}>{value.value}</li>) }
-        </ul>
-      </header>
-    </div>
+      <StudentSignIn></StudentSignIn>
+     </div>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import {render} from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import InstructorSignIn from './pages/signinviews/instructorsignin';
+import InstructorSignUp from './pages/signinviews/instructorsignup';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+render( //browserrouter and routes allows react router to move views based on what the URL extension is
+    <BrowserRouter> 
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/studentsignin" element={<App />} />
+            <Route path="/instructorsignin" element={<InstructorSignIn/>} />
+            <Route path="/instructorsignup" element={<InstructorSignUp />} />
+        </Routes>
+    </BrowserRouter>, 
+    rootElement
+);
