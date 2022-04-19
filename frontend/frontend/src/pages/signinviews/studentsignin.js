a9201ee6cf936fff8150c67a1cf275d15fd1de1c
Grant-Stoehr <54612499+Grant-Stoehr@users.noreply.github.com>
2022-04-18 21:45:35 -0500

    Add files via upload

---
+++ frontend/frontend/src/pages/signinviews/studentsignin.js
@@ -0,0 +1,121 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { createTheme } from '@mui/material/styles';
+import Tooltip from '@mui/material/Tooltip';
+import './studentsignin.css'
+import {Link} from "react-router-dom";
+
+export default function StudentSignIn() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    return (
+        <Grid container component="main" sx={{ height: '100vh' }}>
+          <CssBaseline />
+          <Grid
+            item
+            xs={false}
+            sm={4}
+            md={7}
+            sx={{
+              backgroundImage: 'url(https://www.smu.edu/-/media/Site/_Lyle/Academics/Departments/CS/CS-Home/CS_Home_Faculty.jpg?h=594&la=en&w=1056&hash=EB7823706804D039080FC55A16317B18)',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          />
+          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+                <img src="https://i.imgur.com/AxiNrn4.png"alt="Guiql Logo"/>    
+              <Typography component="h1" variant="h5">
+                Student Sign in
+              </Typography>
+              <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address or Username"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                {/* <FormControlLabel */}
+                  {/* control={<Checkbox value="remember" color="primary" />} */}
+                  {/* label="Remember me" */}
+                {/* /> */}
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Grid container> 
+                <Grid item xs>
+                    <Tooltip title="Student accounts are created by their instructor. Please contact your instructor for your student account.">
+                        <h3>
+                         Where is my student account?
+                        </h3>
+                    </Tooltip>    
+                </Grid>
+                </Grid>
+                <br></br>
+                <br></br> 
+             <Typography component="h1" variant="h5">
+                Instructors 
+              </Typography>
+              <br></br>
+                <Grid container>
+                     <Grid item xs>
+                    <Link to="/instructorsignin">
+                      <Button variant="contained">
+                      Instructor Sign In  
+                      </Button>
+                    </Link>
+                  </Grid>
+                  <Grid item xs>
+                    <Link to="/instructorsignup">
+                      <Button variant="contained">
+                      Instructor Sign Up
+                      </Button>
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+    );
+}
