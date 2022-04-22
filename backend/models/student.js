97f007944da10a6d202a55961921952122eac811
snehagalex <71729811+snehagalex@users.noreply.github.com>
2022-04-21 19:29:20 -0500

    Update student.js
    added methods to create student and authenticate student

--- backend/models/student.js
+++ backend/models/student.js
@@ -1,6 +1,47 @@
 const knex = require('../database/knex');
+const bcrypt = require('bcrypt');
+const e = require('express');
 
 const STUDENT_TABLE = 'Student';
+
+const createStudent = async (student_id, first_name, last_name, username, password) => {
+    // check if student already exists
+    const id = await searchByID(student_id);
+    const userName = await searchByUsername(username);
+    const eMail = await searchByEmail(email);
+
+    if (id) {
+        return "Student has already been added";
+    } else if (userName) {
+        return "Username taken!";
+    } else if (eMail) {
+        return "Email already associated with another account!";
+    } else {  //if student does not already exist, add their info to the table
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+        const query = await knex(STUDENT_TABLE).insert({student_id,first_name,last_name,email,username,password: hashedPassword,salt});
+        const returnValue = await knex(STUDENT_TABLE).select('Student.student_id','Student.first_name','Student.last_name','Student.email','Student.username');
+        return returnValuel
+    }
+}
+
+const authenticate = async (username,password) => {
+    const validUsername = await searchByUsername(username);
+
+    // check if username exists
+    if (validUsername == false) {
+        return "Username does not exist!";
+    } else {
+        // check if password is correct
+        const validPassword = await findUserByPassword(username,password);
+         if (validPassword.length !== 0) {;
+            const query = await knex(USER_TABLE).where({username,password: validPassword[0].password});
+            return query;
+        } else {
+            return "Password is incorrect!";
+        }
+    }
+}
 
 const searchByID = async (student_id) => {
     const query = await knex(STUDENT_TABLE).where({ student_id });
@@ -45,6 +86,8 @@ const removeStudent = async (student_id) => {
 }
 
 module.exports = {
+    createStudent,
+    authenticate,
     searchByID,
     searchByEmail,
     searchByUsername,
