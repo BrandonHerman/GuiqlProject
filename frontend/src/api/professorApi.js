4ca5afb1ddef93326053c8da4a15a403c7dceb44
BrandonHerman <brandonherman0202@gmail.com>
2022-04-27 15:52:56 -0500

    Repository test

--- frontend/src/api/professorApi.js
+++ frontend/src/api/professorApi.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { hostname } from './repositoryConfig';
 
-
-
 // I need to get the professors based on their university
 export const getProfessorByUniversity = (university) => new Promise((resolve, reject) => {
     axios.get(`${hostname}/${university}`, apiConfig)
@@ -12,3 +10,4 @@ export const getProfessorByUniversity = (university) => new Promise((resolve, re
             reject(x);
         });
 });
+
