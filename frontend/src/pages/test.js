f9eb926ae5dcf590687f2ad232a32ac8724f287e
Grant Stoehr <grantstoehr@Grants-MacBook-Pro.local>
2022-04-28 11:33:36 -0500

    CHanges to code

--- frontend/src/pages/test.js
+++ frontend/src/pages/test.js
@@ -1,14 +1,39 @@
 import * as React from 'react';
 import testStuff from '../testStuff.json';
+import testStuff2 from '../testStuff2.json';
 
 
 function Test() {
-    var json = JSON.parse(JSON.stringify(testStuff));
-    // console.log(json.profs[0].classes[1].students[0].firstName);
+    const fs = require('fs');
+    //var json = fs.readFileSync(testStuff2);
+    //var myObject= JSON.parse(json);
+
+    var json = JSON.parse(testStuff2);
+    json.profs[0].classes[0].students.push({
+        "firstName": "Grantham",
+        "lastName": "BingBong",
+        "username": "GBingBong",
+        "password": "dummy",
+        "email": "GBingBong@smu.edu",
+        "inTeam": true,
+        "teamID": 3
+    });
+    var obj = JSON.stringify(json);
+    fs.writeFileSync(testStuff2, obj);
+
+
+
+    //console.log(newStudent);
+    // Add the new student to the class 1
+    //json.profs[0].classes[0].students.push(newStudent);
+
+    // Print the full json file
+    console.log(json);
+
+
+
+    //fs.writeFile("testStuff2.json", JSON.stringify(json))
     // console.log()
-    
-
-
 
     
     
@@ -18,12 +43,12 @@ function Test() {
     // var classes = prof.classes;
     // console.log(classes);
 
-    //return (
-        //<div>
-            {/*<h1>Test</h1>*/}
+    return (
+        <div>
+            <h1>Test</h1>
             {/*<button onClick={getStudentsByProfClassTeamId(1, 2, 2)}>Get Prof</button>*/}
-        //</div>
-     //);
+        </div>
+     );
 }
 
 
