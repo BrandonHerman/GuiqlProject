4f6ff5d49dbaafa3966735f72242fb4164a90c47
snehagalex <71729811+snehagalex@users.noreply.github.com>
2022-04-28 04:32:25 -0500

    Update user_story.js

--- backend/models/user_story.js
+++ backend/models/user_story.js
@@ -3,7 +3,7 @@ const knex = require('../database/knex');
 const STORY_TABLE = 'User_story';
 
 const createUserStory = async (title,description) => {
-    const query = await knex(STORY_TABLE).insert({status:false,title,description,favorite:false});
+    const query = await knex(STORY_TABLE).insert({status:'to do',title,description,favorite:false});
     const returnValue = await knex(STORY_TABLE).select('User_story.story_id','User_story.title','User_story.description');
     return returnValue;
 }
