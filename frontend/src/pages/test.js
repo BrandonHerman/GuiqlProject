79b62d97437e9346d1a4bddbd6ee3c37f817142b
Grant Stoehr <grantstoehr@Grants-MacBook-Pro.local>
2022-04-28 07:19:05 -0500

    restructuring

--- frontend/src/pages/test.js
+++ frontend/src/pages/test.js
@@ -7,24 +7,7 @@ function Test() {
     console.log(json.profs[0].classes[1].students[0].firstName);
     console.log()
 
-    function profSignIn(inUsername, inPassword){
-        // FIND PROFESSOR GIVEN USERNAME
-        for(let i = 0; i < json.profs.length; i++){
-            if(json.profs[i].username === inUsername){
-                if(json.profs[i].password === inPassword){
-                    console.log("Successful Login");
-                    // InstructorProfile.setEmail(prof[i].email);
-                    // InstructorProfile.setName(prof[i].firstName, prof[i].lastName);
-                    // InstructorProfile.setID(prof[i].id);
-                    // InstructorProfile.setUsername(prof[i].username);
-                    // InstructorProfile.setPassword(prof[i].password);
-                    // navigate('/classeshome');
-                    return;
-                }
-            }
-        }
-        return alert("Invalid username or password");
-    }
+    
     
     function studentSignIn(inUsername, inPassword){
         // FIND STUDENT GIVEN USERNAME
