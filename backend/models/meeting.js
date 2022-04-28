e3108ced14cc6d7d9e691b56f27d36ff5a917de3
snehagalex <71729811+snehagalex@users.noreply.github.com>
2022-04-27 19:34:54 -0500

    Update meeting.js

--- backend/models/meeting.js
+++ backend/models/meeting.js
@@ -2,17 +2,10 @@ const knex = require('../database/knex');
 
 const MEETING_TABLE = 'Meeting';
 
-const createMeeting = async (meeting_id,meeting_time,meeting_place,team_id) => {
-    //check if team already exists
-    const id = await searchByID(meeting_id);
-
-    if(id) {
-        return "Meeting already created";
-    } else {
-        const query = await knex(MEETING_TABLE).insert({meeting_id,meeting_time,meeting_place,team_id});
-        const returnValue = await knex(MEETING_TABLE).select('Meeting.meeting_id','Meeting.meeting_time','Meeting.meeting_place','Meeting.team_id');
-        return returnValue;
-    }
+const createMeeting = async (meeting_time,meeting_place,team_id) => {
+    const query = await knex(MEETING_TABLE).insert({meeting_time,meeting_place,team_id});
+    const returnValue = await knex(MEETING_TABLE).select('Meeting.meeting_id','Meeting.meeting_time','Meeting.meeting_place','Meeting.team_id');
+    return returnValue;
 }
 
 const searchByTeam = async (team_id) => {
