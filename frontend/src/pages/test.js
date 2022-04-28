75844cb1c9efb7823de0d5223b305c9dc691b1e9
Grant Stoehr <grantstoehr@Grants-MacBook-Pro.local>
2022-04-28 12:16:04 -0500

    JSON userStories

--- frontend/src/pages/test.js
+++ frontend/src/pages/test.js
@@ -4,31 +4,39 @@ import testStuff2 from '../testStuff2.json';
 
 
 function Test() {
-    const fs = require('fs');
-    //var json = fs.readFileSync(testStuff2);
-    //var myObject= JSON.parse(json);
+    // const fs = require('fs');
+    // //var json = fs.readFileSync(testStuff2);
+    // //var myObject= JSON.parse(json);
 
-    var json = JSON.parse(testStuff2);
-    json.profs[0].classes[0].students.push({
-        "firstName": "Grantham",
-        "lastName": "BingBong",
-        "username": "GBingBong",
-        "password": "dummy",
-        "email": "GBingBong@smu.edu",
-        "inTeam": true,
-        "teamID": 3
-    });
-    var obj = JSON.stringify(json);
-    fs.writeFileSync(testStuff2, obj);
+    // var json = JSON.parse(testStuff2);
+    // json.profs[0].classes[0].students.push({
+    //     "firstName": "Grantham",
+    //     "lastName": "BingBong",
+    //     "username": "GBingBong",
+    //     "password": "dummy",
+    //     "email": "GBingBong@smu.edu",
+    //     "inTeam": true,
+    //     "teamID": 3
+    // });
+    // var obj = JSON.stringify(json);
+    // fs.writeFileSync(testStuff2, obj);
 
+    // const fs = require('fs');
+  
+    // let data = "This is a file containing a collection of books.";
+      
+    // fs.writeFile(testStuff.json, data, (err) => {
+    //   if (err)
+    //     console.log(err);
+    //   else {
+    //     console.log("File written successfully\n");
+    //     console.log("The written has the following contents:");
+    //     console.log(fs.readFileSync("books.txt", "utf8"));
+    //   }
+    // });
 
-
-    //console.log(newStudent);
-    // Add the new student to the class 1
-    //json.profs[0].classes[0].students.push(newStudent);
-
-    // Print the full json file
-    console.log(json);
+    // // Print the full json file
+    // console.log(json);
 
 
 
