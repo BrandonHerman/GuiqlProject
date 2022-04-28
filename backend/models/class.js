5d9f82e3bc793de4e51c4a6063d7298b96aaa79a
snehagalex <71729811+snehagalex@users.noreply.github.com>
2022-04-28 00:34:10 -0500

    Update class.js

--- backend/models/class.js
+++ backend/models/class.js
@@ -2,8 +2,8 @@ const knex = require('../database/knex');
 
 const CLASS_TABLE = 'Class';
 
-const createClass = async (class_time,size,group_count,prof_id,college_id) => {
-    const query = await knex(CLASS_TABLE).insert({class_time,size,group_count,prof_id,college_id});
+const createClass = async (class_time,prof_id,group_count,size,college_id) => {
+    const query = await knex(CLASS_TABLE).insert({class_time,prof_id,group_count,size,college_id});
     const returnValue = await knex(CLASS_TABLE).select('Class.class_id','Class.class_time','Class.prof_id','Class.college_id');
     return returnValue;
 }
