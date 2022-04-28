5acd2a979ef8cb29b469e1e448ab01131f215d92
papriabhuiyan <56096609+papriabhuiyan@users.noreply.github.com>
2022-04-28 01:01:43 -0500

    added class_type

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -6,7 +6,7 @@ router.post('/createStudent', async (req, res, next) => {
     try {
         const body = req.body;
         console.log(body);
-        const result = await req.models.student.createStudent( body.username, body.password,body.first_name, body.last_name, body.email, body.prof_id,body.class_id, body.college_id);
+        const result = await req.models.student.createStudent( body.username, body.password,body.first_name, body.last_name, body.email, body.class_type, body.prof_id,body.class_id, body.college_id);
         res.status(201).json(result);
     } catch (err) {
         console.error('Failed to create new Student:', err);
